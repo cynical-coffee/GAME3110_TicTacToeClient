c85ae380456fbf344ce29ccfa545764b3946bc44
Meghan <mparis003@gmail.com>
2022-12-15 15:16:44 -0500

    System can now check for winner.

--- Assets/Scripts/StateManager.cs
+++ Assets/Scripts/StateManager.cs
@@ -5,17 +5,15 @@ using UnityEngine;
 
 public static class Signifiers
 {
-    // Server to Client
-    public const int LoggedInSignifier = 2;
-    public const int CreatedRoomSignifier = 4;
-    public const int StartGameSignifier = 6;
-
-    // Client to Server
     public const int RegisterAccountSignifier = 0;
     public const int LoginAccountSignifier = 1;
+    public const int LoggedInSignifier = 2;
     public const int CreateRoomSignifier = 3;
+    public const int CreatedRoomSignifier = 4;
     public const int LeaveRoomSignifier = 5;
+    public const int StartGameSignifier = 6;
     public const int GamePlaySignifier = 7;
+    public const int WinnerSignifier = 8;
 }
 
 public class StateManager : MonoBehaviour
--- Assets/Scripts/TicTacToeManager.cs
+++ Assets/Scripts/TicTacToeManager.cs
@@ -12,6 +12,7 @@ public class TicTacToeManager : MonoBehaviour
     private Button[] buttonSquares;
     private string player = "";
     private bool isPlayerTurn;
+    private int[] gameBoard;
     [SerializeField] private GameObject buttonGrid;
 
     public enum GameState
@@ -34,7 +35,7 @@ public class TicTacToeManager : MonoBehaviour
             int k = i;
             buttonSquares[i].onClick.AddListener(delegate { PickSlot(k); });
         }
-
+        gameBoard = new[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
         state = GameState.START;
         WaitForOpponent();
     }
@@ -68,6 +69,8 @@ public class TicTacToeManager : MonoBehaviour
         buttonSquares[buttonNum].GetComponentInChildren<TextMeshProUGUI>().text = player;
         client.SendMessageToHost(Signifiers.GamePlaySignifier + "," + player + "," + buttonNum.ToString());
         buttonSquares[buttonNum].interactable = false;
+        gameBoard[buttonNum] = 1;
+        CheckWinConditions();
         state = GameState.OPPONENTTURN;
     }
 
@@ -92,11 +95,39 @@ public class TicTacToeManager : MonoBehaviour
         }
     }
 
-    public void GetPlayerLetter(string receivedMessage)
+    private void CheckWinConditions()
     {
-        string[] playerLetter = receivedMessage.Split(",");
-        Debug.Log(playerLetter);
-        player = playerLetter[1];
+        int row1 = gameBoard[0] + gameBoard[1] + gameBoard[2];
+        int row2 = gameBoard[3] + gameBoard[4] + gameBoard[5];
+        int row3 = gameBoard[6] + gameBoard[7] + gameBoard[8];
+
+        int col1 = gameBoard[0] + gameBoard[3] + gameBoard[6];
+        int col2 = gameBoard[1] + gameBoard[4] + gameBoard[7];
+        int col3 = gameBoard[2] + gameBoard[5] + gameBoard[8];
+
+        int cross1 = gameBoard[0] + gameBoard[4] + gameBoard[8];
+        int cross2 = gameBoard[2] + gameBoard[4] + gameBoard[6];
+
+        Debug.Log($"row 1: {row1}");
+        Debug.Log($"row 2: {row2}");
+        Debug.Log($"row 3: {row3}");
+
+        Debug.Log($"col 1: {col1}");
+        Debug.Log($"col 2: {col2}");
+        Debug.Log($"col 3: {col3}");
+
+        Debug.Log($"cross 1: {col3}");
+        Debug.Log($"cross 2: {col3}");
+
+        int[] solutions = new int[] { row1, row2, row3, col1, col2, col3, cross1, cross2 };
+        for (int i = 0; i < solutions.Length; i++)
+        {
+            if (solutions[i] == 3)
+            {
+                Debug.Log("Player Wins!" + solutions[i] + player);
+                client.SendMessageToHost(Signifiers.WinnerSignifier.ToString());
+            }
+        }
     }
 
     public void ProcessOpponentMove(string receivedMessage)
@@ -110,5 +141,12 @@ public class TicTacToeManager : MonoBehaviour
         int slot = Int32.Parse(opponentMove[2]);
         buttonSquares[slot].GetComponentInChildren<TextMeshProUGUI>().text = opponentMove[1];
         state = GameState.PLAYERTURN;
+    }
+
+    public void GetPlayerLetter(string receivedMessage)
+    {
+        string[] playerLetter = receivedMessage.Split(",");
+        Debug.Log(playerLetter);
+        player = playerLetter[1];
     }
 }
